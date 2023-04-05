3b27c015d5ff4cb5a2c55437caa34e85bdedc404
Aleksandr <aleksandr.evsyunin>
2023-04-05 09:42:35 +0600

    add strict function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,15 @@
+import { strict as assert } from 'node:assert';
 import capitalize from "../src/capitalize.js";
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
+
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'),'Hello');
+
+// if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Функция работает неверно(с переданным аргументом)!');
+//   }
   
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
+//   if (capitalize('') !== '') {
+//     throw new Error('Функция работает неверно!(связано с пустым значением');
+//   }
   
-  console.log('Все тесты пройдены!');
+//   console.log('Все тесты пройдены!');
